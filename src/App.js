3296763bcb2c67cf21e602f632784cb8280c06f7
Aochocinco <achalurankar@gmail.com>
2022-12-08 00:09:59 +0530

    api-try

--- src/App.js
+++ src/App.js
@@ -26,3 +26,18 @@ function App() {
 }
 
 export default App;
+
+console.log('initiating request')
+fetch('https://playful-otter-5orsso-dev-ed.my.salesforce.com/services/oauth2/token?grant_type=password&client_id=3MVG9pRzvMkjMb6mVmRLS1jKALgWzMgGDQhnBh4MfyEQv_ATznjlpPw9ffz3Ur.ZxRF3K6347v7GnsdgUeuJh&client_secret=6781FB6C8723F54B5C352552D8386E7ADD2CFB9F9D863C627F15155AF7E82DA4&username=achal_urankar@playful-otter-5orsso.com&password=achal123BMcp5piJtsoVWlOZd5q9WQs7', {
+    method : "POST"
+})
+.then(res => res.json())
+.then(res => {
+    console.log('in then')
+    console.log('res', res)
+    console.log('stringified', JSON.stringify(res))
+}).catch(err => {
+    console.log('in catch')
+    console.log('err', err)
+    console.log('stringified', JSON.stringify(err))
+})
